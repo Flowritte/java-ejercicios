734f900b0e72cad49d227253e2781770085a1668
Hector Faraday <143033970+Flowritte@users.noreply.github.com>
2023-08-25 16:24:24 -0600

    Add files via upload

---
+++ contraseñas/generador.java
@@ -0,0 +1,13 @@
+package contraseñas;
+
+
+
+public class generador {
+	
+	public static void main(String[] args) {
+		constrasenias.getpassword(
+				constrasenias.mayusculas+constrasenias.minusculas+constrasenias.numeros,10);
+		
+	}
+	
+}
